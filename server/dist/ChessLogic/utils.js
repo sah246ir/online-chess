935879cefa4488c88bfba86c69b1cbbe2d38a1fe
sahir <sahir@pop-os.localdomain>
2024-05-16 12:42:55 +0530

    chore: removing ts compilation files

--- server/dist/ChessLogic/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseNotation = void 0;
-function parseNotation(notation) {
-    let row = 8 - parseInt(notation.charAt(1));
-    let col = notation.charCodeAt(0) - 97;
-    return [row, col];
-}
-exports.parseNotation = parseNotation;
